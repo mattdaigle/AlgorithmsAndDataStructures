40278acabf3f0f4cf4c0c5e1845d90a1a05232fe
mattdaigle <9753749+mattdaigle@users.noreply.github.com>
2024-07-14 13:46:24 -0400

    Added Linked List

---
+++ AlgorithmsAndDataStructures/App/AlgorithmsAndDataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmsAndDataStructuresApp.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Matt Daigle on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmsAndDataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/LinkedList.swift
@@ -0,0 +1,104 @@
+//
+//  LinkedList.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Matt Daigle on 7/14/24.
+//
+
+import Foundation
+
+class Node {
+
+    var value: Int
+    var next: Node?
+
+    init(value: Int) {
+        self.value = value
+    }
+}
+
+class LinkedList {
+
+    private var head: Node?
+
+    init(head: Node? = nil) {
+        self.head = head
+    }
+
+    func append(_ node: Node) {
+        guard head != nil else {
+            head = node
+            return
+        }
+
+        var current = head
+
+        while current?.next != nil {
+            current = current?.next
+        }
+        current?.next = node
+    }
+
+    func getNode(atPosition position: Int) -> Node? {
+        guard position > 0 else {
+            return nil
+        }
+
+        var current = head
+
+        for counter in 1...position {
+            if counter == position || current == nil {
+                break
+            }
+
+            current = current?.next
+        }
+
+        return current
+    }
+
+    func insert(_ node: Node, at position: Int) {
+        guard position > 0 else {
+            return
+        }
+
+        guard position > 1 else {
+            node.next = head
+            head = node
+            return
+        }
+
+        var current = head
+
+        for counter in 1..<position {
+            if counter == position - 1 || current == nil {
+                break
+            }
+
+            current = current?.next
+        }
+
+        node.next = current?.next
+        current?.next = node
+    }
+
+    func deleteFirstNode(withValue value: Int) {
+        var current = head
+        var previous: Node?
+
+        while current?.value != value && current?.next != nil {
+            previous = current
+            current = current?.next
+        }
+
+        guard current?.value == value else {
+            return
+        }
+
+        if let previous {
+            previous.next = current?.next
+        } else {
+            head = current?.next
+        }
+    }
+}
